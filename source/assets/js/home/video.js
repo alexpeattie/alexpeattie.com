a2a28c39f3375ac1c27bf22b7de0fb83ca6864e5
Alex Peattie <alexpeattie@gmail.com>
2016-03-17 20:14:20 +0000

    Lazy loading videos

--- source/assets/js/home/video.js
+++ source/assets/js/home/video.js
@@ -1,10 +1,29 @@
 //= require video.js/dist/video-js/video
 //= require remodal/dist/remodal
 
-$(document).on('opening', '.remodal', function () {
-  var vidId = $(this).find('video').attr('id');
-  videojs(vidId).play();
-}).on('closing', '.remodal', function () {
-  var vidId = $(this).find('video').attr('id');
-  videojs(vidId).pause();
-});
+$(document).ready(function() {
+  var videoModal;
+  var remodalInstance;
+
+  $(document).on('closing', '.remodal', function (e) {
+    videojs($('[data-remodal-id] video')[0]).pause();
+  })
+
+  $('[video-modal]').on('click', function() {
+    $('[data-remodal-id]').remove();
+
+    var modalHtml = $('#video-modal-template').html()
+    var vidName = $(this).attr('video-modal');
+
+    var modal = $(modalHtml)
+      .find('video').attr('id', 'video-' + vidName).end()
+      .find('source').attr('src', '/assets/videos/' + vidName + '.mp4').end()
+      .appendTo('body')
+
+    var video = videojs(modal.find('video')[0], {});
+    var remodal = modal.remodal()
+    remodal.open();
+
+    video.play();
+  })
+})
